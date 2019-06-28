45a5d86447b50fd68be7a3bbf1740cb7f72ee08c
Le Bao Hiep <baohiep2013@gmail.com>
2019-06-28 14:57:39 +0700

    Switch to IOHIDEventService and add some functions

--- AsusHID/AsusHID.cpp
+++ AsusHID/AsusHID.cpp
@@ -8,10 +8,10 @@
 #include "AsusHID.hpp"
 
 #define super IOService
-OSDefineMetaClassAndStructors(AsusHID, IOService);
+OSDefineMetaClassAndStructors(AsusHID, IOHIDEventService);
 
-bool AsusHID::start(IOService *provider) {
-    DBGLOG("hid", "start is called");
+bool AsusHID::handleStart(IOService *provider) {
+    DBGLOG("hid", "handleStart is called");
 
     if (!super::start(provider)) {
         SYSLOG("hid", "Error loading HID driver");
@@ -29,17 +29,28 @@ bool AsusHID::start(IOService *provider) {
         DBGLOG("hid", "Connected with AsusSMC");
     }
 
+    hid_interface = OSDynamicCast(IOHIDInterface, provider);
+    if (!hid_interface)
+        return false;
+
+    if (!hid_interface->open(this, 0, OSMemberFunctionCast(IOHIDInterface::InterruptReportAction, this, &AsusHID::handleInterruptReport), NULL))
+        return false;
+
+    hid_device = OSDynamicCast(IOHIDDevice, hid_interface->getParentEntry(gIOServicePlane));
+    if (!hid_device)
+        return false;
+
     setProperty("Copyright", "Copyright © 2019 hieplpvip");
-    setProperty(kIOHIDTransportKey, provider->copyProperty(kIOHIDTransportKey));
-    setProperty(kIOHIDManufacturerKey, provider->copyProperty(kIOHIDManufacturerKey));
-    setProperty(kIOHIDProductKey, provider->copyProperty(kIOHIDProductKey));
-    setProperty(kIOHIDLocationIDKey, provider->copyProperty(kIOHIDLocationIDKey));
-    setProperty(kIOHIDVersionNumberKey, provider->copyProperty(kIOHIDVersionNumberKey));
+    setProperty(kIOHIDTransportKey, hid_interface->copyProperty(kIOHIDTransportKey));
+    setProperty(kIOHIDManufacturerKey, hid_interface->copyProperty(kIOHIDManufacturerKey));
+    setProperty(kIOHIDProductKey, hid_interface->copyProperty(kIOHIDProductKey));
+    setProperty(kIOHIDLocationIDKey, hid_interface->copyProperty(kIOHIDLocationIDKey));
+    setProperty(kIOHIDVersionNumberKey, hid_interface->copyProperty(kIOHIDVersionNumberKey));
     return true;
 }
 
-void AsusHID::stop(IOService *provider) {
-    DBGLOG("hid", "stop is called");
+void AsusHID::handleStop(IOService *provider) {
+    DBGLOG("hid", "handleStop is called");
 
     if (asusSMC) {
         asusSMC->message(kDelAsusHID, this);
@@ -47,5 +58,45 @@ void AsusHID::stop(IOService *provider) {
     }
     OSSafeReleaseNULL(asusSMC);
 
+    hid_interface->close(this);
+
     super::stop(provider);
+}
+
+void AsusHID::handleInterruptReport(AbsoluteTime timestamp, IOMemoryDescriptor* report, IOHIDReportType report_type, UInt32 report_id) {
+    DBGLOG("hid", "handleInterruptReport is called");
+}
+
+#pragma mark -
+#pragma mark HID functions ported from Linux
+#pragma mark -
+
+void AsusHID::asus_kbd_init() {
+    uint8_t buf[] = { FEATURE_KBD_REPORT_ID, 0x41, 0x53, 0x55, 0x53, 0x20, 0x54,
+        0x65, 0x63, 0x68, 0x2e, 0x49, 0x6e, 0x63, 0x2e, 0x00 };
+    IOBufferMemoryDescriptor* report = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, sizeof(buf));
+    report->writeBytes(0, &buf, sizeof(buf));
+    hid_interface->setReport(report, kIOHIDReportTypeFeature, FEATURE_KBD_REPORT_ID);
+    report->release();
+}
+
+void AsusHID::asus_kbd_backlight_set(uint8_t val) {
+    uint8_t buf[] = { FEATURE_KBD_REPORT_ID, 0xba, 0xc5, 0xc4, val };
+    IOBufferMemoryDescriptor* report = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, sizeof(buf));
+    report->writeBytes(0, &buf, sizeof(buf));
+    hid_interface->setReport(report, kIOHIDReportTypeFeature, FEATURE_KBD_REPORT_ID);
+    report->release();
+}
+
+void AsusHID::asus_kbd_get_functions(unsigned char *kbd_func) {
+    uint8_t buf[] = { FEATURE_KBD_REPORT_ID, 0x05, 0x20, 0x31, 0x00, 0x08 };
+    IOBufferMemoryDescriptor* report = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, sizeof(buf));
+    report->writeBytes(0, &buf, sizeof(buf));
+    hid_interface->setReport(report, kIOHIDReportTypeFeature, FEATURE_KBD_REPORT_ID);
+    report->release();
+
+    report = IOBufferMemoryDescriptor::inTaskWithOptions(kernel_task, 0, FEATURE_KBD_REPORT_SIZE);
+    hid_interface->getReport(report, kIOHIDReportTypeFeature, FEATURE_KBD_REPORT_ID);
+    report->readBytes(6, kbd_func, 1);
+    report->release();
 }
--- AsusHID/AsusHID.hpp
+++ AsusHID/AsusHID.hpp
@@ -8,22 +8,36 @@
 #ifndef AsusHID_hpp
 #define AsusHID_hpp
 
+#include <IOKit/hid/IOHIDDevice.h>
 #include <IOKit/hid/IOHIDKeys.h>
+#include <IOKit/hidevent/IOHIDEventService.h>
 #include <VirtualSMCSDK/kern_vsmcapi.hpp>
+
+#define FEATURE_KBD_REPORT_ID 0x5a
+#define FEATURE_KBD_REPORT_SIZE 16
 
 enum {
     kAddAsusHID = iokit_vendor_specific_msg(8102),
     kDelAsusHID = iokit_vendor_specific_msg(7501),
 };
 
-class AsusHID : public IOService {
+class AsusHID : public IOHIDEventService {
     OSDeclareDefaultStructors(AsusHID)
 
 public:
-    virtual bool start(IOService *provider) override;
-    virtual void stop(IOService *provider) override;
+    virtual bool handleStart(IOService *provider) override;
+    virtual void handleStop(IOService *provider) override;
 
 private:
     IOService *asusSMC {nullptr};
+    IOHIDDevice* hid_device {nullptr};
+    IOHIDInterface* hid_interface {nullptr};
+
+    void handleInterruptReport(AbsoluteTime timestamp, IOMemoryDescriptor* report, IOHIDReportType report_type, UInt32 report_id);
+
+    // Ported from hid-asus driver
+    void asus_kbd_init();
+    void asus_kbd_backlight_set(uint8_t val);
+    void asus_kbd_get_functions(unsigned char *kbd_func);
 };
 #endif /* AsusHID_hpp */
